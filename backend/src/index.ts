7b82888a67b03c599b74da9e160e8cdaab6dc4bf
JvTadeo <joao.tadeot2@gmail.com>
2025-04-05 23:24:58 -0300

    Change some description

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,12 +6,17 @@ import cors from 'cors'; //identifica o endereço IP
 dotenv.config();
 
 const app = express();
+
+// TODO o SERVIDOR precisa de uma PORTA
 const port = process.env.PORT; //todo servidor precisa de uma, padrão = porta 3000
 
 app.use(cors())
 app.use('/', router);
 
-app.listen(port, () => { // app..listen(port, minhafuncaoparaligaroserver)
+app.listen(port, () => {
+    // Port é uma variável de ambiente, que vai ser pega no .ENV
+    // .ENV é o local onde vamos colocar as variáveis sensiveis
+
     console.log('funciona');
     console.log(port);
 });
